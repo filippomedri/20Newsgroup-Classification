c75a0cc978c1bbd44cd0a6a8e33f13c17652204a
filippomedri <filippo.medri@gmail.com>
2018-06-04 14:10:50 -0700

    Updated kmeans model

--- kmeans_model.py
+++ kmeans_model.py
@@ -3,67 +3,51 @@ import pickle
 
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import HashingVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import Normalizer
 from sklearn import metrics
 
 from sklearn.cluster import KMeans, MiniBatchKMeans
-
-import logging
-import sys
 from time import time
 
-import numpy as np
-
 class KMeansModel(object):
-
-    def __init__(self,dataset,modeling_list,
-                 use_hashing,use_idf,
+    def __init__(self,dataset,
+                 modeling_list,
                  n_clusters,
                  n_features,
-                 verbose
                  ):
+        '''
+        :param dataset          : pandas dataframe for data I/O
+        :param modeling_list    : column of text to be used by clustering
+        :param n_clusters       : number of cluster for the k-means algorithm
+        :param n_features       : number of features for the count matrix
+        '''
         self.dataset = dataset
         self.data = dataset[modeling_list]
         self.n_features = n_features
-        self.use_hashing = use_hashing
-        self.use_idf = use_idf
-        self.verbose = verbose
         self.n_clusters = n_clusters
         self.labels = dataset.category.astype("category").cat.codes
 
-
-
     def vectorize(self):
+        '''
+        Create tfidf matrix
+        '''
         print("Extracting features from the training dataset using a sparse vectorizer")
-
         t0 = time()
-        if self.use_hashing:
-            if self.use_idf:
-                # Perform an IDF normalization on the output of HashingVectorizer
-                hasher = HashingVectorizer(n_features=self.n_features,
-                                           stop_words='english', alternate_sign=False,
-                                           norm=None, binary=False)
-                self.vectorizer = make_pipeline(hasher, TfidfTransformer())
-            else:
-                self.vectorizer = HashingVectorizer(n_features=self.n_features,
-                                               stop_words='english',
-                                               alternate_sign=False, norm='l2',
-                                               binary=False)
-        else:
-            self.vectorizer = TfidfVectorizer(max_df=0.5, max_features=self.n_features,
-                                         min_df=2, stop_words='english',
-                                         use_idf=self.use_idf)
+        self.vectorizer = TfidfVectorizer(max_df=0.5, max_features=self.n_features,
+                                         min_df=2, stop_words='english')
         self.X = self.vectorizer.fit_transform(self.data)
-
         print("done in %fs" % (time() - t0))
         print("n_samples: %d, n_features: %d" % self.X.shape)
         print()
 
 
     def apply_lsa(self,n_components):
+        '''
+        Perform LSA
+        :param n_components: number of components for LSA
+        :return:
+        '''
         self.n_components = n_components
         print("Performing dimensionality reduction using LSA")
         t0 = time()
@@ -83,9 +67,7 @@ class KMeansModel(object):
         self.dataset['component_2'] = self.X[:, 1].copy()
         self.dataset['component_3'] = self.X[:, 2].copy()
 
-
         print("done in %fs" % (time() - t0))
-
 
         explained_variance = self.svd.explained_variance_ratio_.sum()
         print("Explained variance of the SVD step: {}%".format(
@@ -93,14 +75,17 @@ class KMeansModel(object):
 
 
     def run(self,miniBatch = False):
+        '''
+        Perform k-means clustering
+        :param miniBatch: Boolean to select between regular k-means and mini batch
+        :return:
+        '''
         self.miniBatch = miniBatch
-
         if self.miniBatch:
             self.km = MiniBatchKMeans(n_clusters=self.n_clusters, init='k-means++', n_init=1,
-                                 init_size=1000, batch_size=1000, verbose=self.verbose)
+                                 init_size=1000, batch_size=1000)
         else:
-            self.km = KMeans(n_clusters=self.n_clusters, init='k-means++', max_iter=100, n_init=1,
-                        verbose=self.verbose)
+            self.km = KMeans(n_clusters=self.n_clusters, init='k-means++', max_iter=100, n_init=1)
 
         print("Clustering sparse data with %s" % self.km)
         t0 = time()
@@ -108,10 +93,15 @@ class KMeansModel(object):
         print("done in %0.3fs" % (time() - t0))
         print()
 
+        # update clustering
         self.dataset['cluster'] = self.km.labels_.copy()
 
 
     def get_metrics(self):
+        '''
+        Print metrics for the clustering
+        Print top term of each cluster
+        '''
         print("Homogeneity: %0.3f" % metrics.homogeneity_score(self.labels, self.km.labels_))
         print("Completeness: %0.3f" % metrics.completeness_score(self.labels, self.km.labels_))
         print("V-measure: %0.3f" % metrics.v_measure_score(self.labels, self.km.labels_))
@@ -122,17 +112,13 @@ class KMeansModel(object):
 
         print()
 
-        if not self.use_hashing:
-            print("Top terms per cluster:")
+        print("Top terms per cluster:")
 
-            if self.n_components:
-                original_space_centroids = self.svd.inverse_transform(self.km.cluster_centers_)
-                order_centroids = original_space_centroids.argsort()[:, ::-1]
-            else:
-                order_centroids = self.km.cluster_centers_.argsort()[:, ::-1]
+        original_space_centroids = self.svd.inverse_transform(self.km.cluster_centers_)
+        order_centroids = original_space_centroids.argsort()[:, ::-1]
 
-            terms = self.vectorizer.get_feature_names()
-            for i in range(self.n_clusters):
+        terms = self.vectorizer.get_feature_names()
+        for i in range(self.n_clusters):
                 print("Cluster %d:" % i, end='')
                 for ind in order_centroids[i, :10]:
                     print(' %s' % terms[ind], end='')
